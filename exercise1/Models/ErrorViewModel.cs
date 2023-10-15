02daa911a5dfde4ac8bd19e35fcc869f2fcb776a
Rmnarj <www.arman.9872@gmail.com>
2023-10-15 22:51:41 +0330

    create project

---
+++ exercise1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace exercise1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
